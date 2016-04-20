6164402635a4877890ab059d56398c4c8afd543f
gnayuy <gnayuy@gmail.com>
2016-04-20 11:46:12 -0400

    update

--- curve2dmap.cc
+++ curve2dmap.cc
@@ -169,7 +169,6 @@ const char* vsScreen =
 "}";
 
 const char* fsScreen =
-"uniform vec4 color;"
 "uniform sampler2D tex0;"
 "varying vec2 texcoord;"
 "void main () {"
@@ -490,25 +489,24 @@ int main(int argc, char *argv[])
         //glDrawBuffer(GL_COLOR_ATTACHMENT0);
         
         //glPopAttrib();
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
         
         // Render to screen
-//        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-//        glViewport(0, 0, dimx, dimy);
-//        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//        
-//        //
-//        glUseProgram(spScn);
-//        glActiveTexture(GL_TEXTURE0);
-//        glBindTexture(GL_TEXTURE_2D, textures[PJTEX]);
-//        glUniform1i(tex_loc, 0);
-//        
-//        //
-//        glEnableVertexAttribArray(0);
-//        glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
-//        glVertexAttribPointer(pos_loc, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-//        glDrawArrays(GL_TRIANGLES, 0, 6);
-//        glDisableVertexAttribArray(0);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glViewport(0, 0, dimx, dimy);
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        //
+        glUseProgram(spScn);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[PJTEX]);
+        glUniform1i(tex_loc, 0);
+        
+        //
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, quad_vertexbuffer);
+        glVertexAttribPointer(pos_loc, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDisableVertexAttribArray(0);
         
         
         // load deformation into deform texture (sampler2D)
